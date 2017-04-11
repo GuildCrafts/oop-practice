128ff8902cc336d74ffed2a0fbb2e08d0f789913
jasvictoria <jasvmurray@gmail.com>
2017-04-11 09:19:48 -0700

    Day 1 Progress

--- jsinfo/helloObject.js
+++ jsinfo/helloObject.js
@@ -1,15 +1,9 @@
-const myBike = {
-  // your code here
-}
-
 let user = {}
 user.name = "John"
 user.surname = "Smith"
-//user.name = "Pete"
-Object.assign (user, {name: "Pete", isAdmin: true})
+user.name = "Pete"
+//Object.assign (user, {name: "Pete", isAdmin: true})
 delete user.name
 //Object.assign (user, {name: "Pete", isAdmin: true})
 
 console.log(user)
-
-module.exports = myBike
